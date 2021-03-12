46e0dc24d5c2bbab36c2fc0e82c001c8203f15ef
sezann <68513585+sezann@users.noreply.github.com>
2021-03-12 15:47:19 +0300

    Правки по критериям Б14, Д20, Д15

--- js/booking-form.js
+++ js/booking-form.js
@@ -1,11 +1,9 @@
-const popup = document.querySelector('.modal');
+const popup = document.querySelector('.booking-form');
 const buttonSwitchPopup = document.querySelector('.sedona-hotels');
 
 
 buttonSwitchPopup.addEventListener('click',function() {
   popup.classList.toggle('modal-close');
-  popup.classList.toggle('popup--open');
-
 });
 
 
